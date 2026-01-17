606fc2a11be015d8f39f50b22ca714863d102dc1
Isuru Shehara <isurushehara@gmail.com>
2026-01-17 17:37:43 +0530

    feat: Implement math module with add and subtract functions

---
+++ demo/modules/math/math.js
@@ -0,0 +1,15 @@
+// private function
+function add(a, b) {
+  return a + b;
+}
+
+// private function
+function subtract(a, b) {
+  return a - b;
+}
+
+// exporting functions
+module.exports = {
+  add,
+  subtract
+};
